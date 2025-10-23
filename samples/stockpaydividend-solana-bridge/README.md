fea5a4525bc0635dd432c5038d7b1bcfce418abd
Szymon Sztuka <szymon.sztuka@r3.com>
2025-10-23 11:59:17 +0100

    Set default notary for issuing party, second notary may be incorrectly added to transaction (stockpaydivided flows assumes a single deployed notary). Add all participants as in stockpaydivided - they are all requires by flow issuing stock (Shareholder/observer). Add basic readme.

---
+++ samples/stockpaydividend-solana-bridge/README.md
@@ -0,0 +1,66 @@
+# Stock Bridging To Solana Sample
+
+This demo aims to demonstrate how to bridge Corda states built with [TokenSDK](https://training.corda.net/libraries/tokens-sdk/) to Solana Network via additional participant "Bridging Authority"
+and Solana Notary.
+An unmodified [Stock Cordapp](https://github.com/corda/samples-kotlin/tree/release/4.12/Tokens/stockpaydividend)
+is deployed to several parties (the same as in [Stock Cordapp](https://github.com/corda/samples-kotlin/tree/release/4.12/Tokens/stockpaydividend) demo).
+
+### Parties
+
+[Stock Cordapp](https://github.com/corda/samples-kotlin/tree/release/4.12/Tokens/stockpaydividend) assumes there are 4 parties:
+* **WayneCo** - creates the stock state.
+* **Shareholder** - owns the stock and bridge shares to Solana Network.
+* **Bank** - issues fiat tokens.
+* **Observer** - monitors all the stocks by keeping a copy of transactions whenever a stock is created or updated.
+
+
+New network participants:
+* **Bridging Authority** - performs bridging by running "Corda-Solana-Toolkit" Cordapp
+* **Solana Notary** - ensures states are created on Solana Network
+
+## Usage
+### Running the nodes
+
+Open a terminal and go to the project root directory and type: (to deploy the nodes using bootstrapper)
+```
+./gradlew clean samples:stockpaydividend-solana-bridge:deployNodes
+```
+Then type: (to run the nodes)
+```
+./samples/stockpaydividend-solana-bridge/build/nodes/runnodes
+```
+
+## Interacting with the nodes
+
+When started via the command line, each node will display an interactive shell:
+
+    Welcome to the Corda interactive shell.
+    Useful commands include 'help' to see what is available, and 'bye' to shut down the node.
+
+    Thu Oct 23 11:54:18 BST 2025>>>
+
+You can use this shell to interact with your node.
+
+### Running the nodes
+
+These steps focus on bridging activities and not on dividend as in [Stock Cordapp](https://github.com/corda/samples-kotlin/tree/release/4.12/Tokens/stockpaydividend) usage.
+
+##### 1. IssueStock - Stock Issuer
+WayneCo creates a StockState and issues some stock tokens associated to the created StockState.
+>On company WayneCo's node, execute <br>`start IssueStock symbol: TEST, name: "Stock, SP500", currency: USD, price: 7.4, issueVol: 500, notary: "O=Notary Service, L=London, C=GB"`
+
+##### 2. MoveStock - Stock Issuer
+WayneCo transfers some stock tokens to the Shareholder.
+>On company WayneCo's node, execute <br>`start MoveStock symbol: TEST, quantity: 100, recipient: Shareholder`
+
+Now at the Shareholder's terminal, we can see that it received 100 stock tokens:
+>On shareholder node, execute <br>`start GetStockBalance symbol: TEST`
+
+##### 3. Bridge To Solana - Stock Issuer
+Shareholder transfers some stock tokens to the Bridging Authority.
+>On company WayneCo's node, execute <br>`start MoveStock symbol: TEST, quantity: 60, recipient: "Bridging Authority"`
+
+Now at the Bridging Authority's terminal, we can see that it received 100 stock tokens:
+>On Bridging Authority node, execute <br>`start GetStockBalance symbol: TEST`
+
+Bridging Authority does not perform asset bridging to Solana yet.
--- samples/stockpaydividend-solana-bridge/build.gradle.kts
+++ samples/stockpaydividend-solana-bridge/build.gradle.kts
@@ -4,7 +4,6 @@ import org.gradle.api.artifacts.MinimalExternalModuleDependency
 import org.gradle.kotlin.dsl.register
 import org.gradle.api.provider.Provider
 
-
 plugins {
     id("default-kotlin")
     alias(libs.plugins.cordformation)
@@ -25,17 +24,19 @@ dependencies {
     cordapp(libs.samples.kotlin.stockpaydividend.workflows)
 }
 
-// Adds passing TOML references for Cordform.nodeDefaults.cordapp property
-fun Node.cordapp(dep: Provider<MinimalExternalModuleDependency>) {
-    val resolvedDep : MinimalExternalModuleDependency = dep.get()
-    cordapp("${resolvedDep.module.group}:${resolvedDep.module.name}:${resolvedDep.versionConstraint.requiredVersion}")
-}
-
 tasks.register<Cordform>("deployNodes") {
 
     dependsOn(
         project(":bridging-token-contracts").tasks.named("jar"),
         project(":bridging-token-workflows").tasks.named("jar")
+    )
+
+    val commonRpcUser =  listOf(
+        mapOf(
+            "user" to "user1",
+            "password" to "test",
+            "permissions" to listOf("ALL")
+        )
     )
 
     nodeDefaults {
@@ -54,7 +55,7 @@ tasks.register<Cordform>("deployNodes") {
         name("O=Notary,L=London,C=GB")
         notary = mapOf(
             "validating" to "false",
-            "serviceLegalName" to "O=Notary Service,L=Zurich,C=CH"
+            "serviceLegalName" to "O=Notary Service,L=London,C=GB"
         )
         p2pPort(10002)
         rpcSettings {
@@ -63,48 +64,83 @@ tasks.register<Cordform>("deployNodes") {
         }
     }
     node {
-        name("O=Solana Notary,L=London,C=GB")
+        name("O=Solana Notary,L=Ashburn,ST=Virginia,C=US")
         notary = mapOf(
             "validating" to "false",
-            "serviceLegalName" to "O=Solana Notary Service,L=Ashburn,ST=Virginia,C=US"
+            "serviceLegalName" to "O=Solana Notary Service,L=Washington,C=US"
             // TODO add Solana settings
         )
-        p2pPort(10004)
+        p2pPort(10019)
         rpcSettings {
-            address("localhost:10005")
-            adminAddress("localhost:10035")
+            address("localhost:10020")
+            adminAddress("localhost:10048")
         }
     }
     node {
-        name("O=Bank,L=Chicago,C=US")
+        name("O=WayneCo,L=SF,C=US")
+        p2pPort(10005)
+        rpcSettings {
+            address("localhost:10006")
+            adminAddress("localhost:10036")
+        }
+        rpcUsers = commonRpcUser
+    }
+    node {
+        name("O=Shareholder,L=New York,C=US")
+        p2pPort(10008)
+        rpcSettings {
+            address("localhost:10009")
+            adminAddress("localhost:10039")
+        }
+        rpcUsers = commonRpcUser
+    }
+    node {
+        name("O=Bank,L=Washington DC,C=US")
         p2pPort(10012)
         rpcSettings {
             address("localhost:10013")
             adminAddress("localhost:10043")
         }
-        rpcUsers = listOf(
-            mapOf(
-                "user" to "user1",
-                "password" to "test",
-                "permissions" to listOf("ALL")
-            )
-        )
+        rpcUsers = commonRpcUser
+    }
+    node {
+        name("O=Observer,L=Washington DC,C=US")
+        p2pPort(10015)
+        rpcSettings {
+            address("localhost:10016")
+            adminAddress("localhost:10046")
+        }
+        rpcUsers = commonRpcUser
     }
     node {
         name("O=Bridging Authority,L=New York,C=US")
-        p2pPort(10014)
+        p2pPort(10017)
         rpcSettings {
-            address("localhost:10015")
-            adminAddress("localhost:10044")
+            address("localhost:10018")
+            adminAddress("localhost:10047")
         }
-        rpcUsers = listOf(
-            mapOf(
-                "user" to "user1",
-                "password" to "test",
-                "permissions" to listOf("ALL")
-            )
-        )
+        rpcUsers = commonRpcUser
         cordapp(project(":bridging-token-contracts"))
         //cordapp(project(":bridging-token-workflows")) //TODO endable once there is a cordapp
     }
+}
+
+// After adding second Notary (Solana Notary) the stock issuance flow may use a wrong notary,
+// set the original notary as the default notary for the cordapp for WayneCo node.
+tasks.register("writeCordappConfig") {
+    dependsOn("deployNodes")
+    doLast {
+            val dir = layout.buildDirectory.dir("nodes/WayneCo/cordapps/config").get().asFile
+            dir.mkdirs()
+            val cfgFile = File(dir, "tokens-workflows-1.3.2.conf")
+            cfgFile.writeText("\"notary\" = \"O=Notary Service,L=London,C=GB\"\n")
+
+    }
+}
+tasks.named("deployNodes") { finalizedBy("writeCordappConfig") }
+
+// Adds passing TOML references for Cordform.nodeDefaults.cordapp property
+fun Node.cordapp(dep: Provider<MinimalExternalModuleDependency>) {
+    val value : MinimalExternalModuleDependency = dep.get()
+    cordapp("${value.module.group}:${value.module.name}:${value.versionConstraint.requiredVersion}")
 }
