c76867b925f86b83e82486d4002a8dc6350f5164
Szymon Sztuka <szymon.sztuka@r3.com>
2025-11-24 16:49:18 +0000

    Added more code ot the test (fails + needs locally build Corda Enterprise from https://github.com/corda/enterprise/pull/5577)

--- mock-network-tests/src/test/kotlin/com/r3/corda/lib/solana/bridging/token/test/driver/DriverTests.kt
+++ mock-network-tests/src/test/kotlin/com/r3/corda/lib/solana/bridging/token/test/driver/DriverTests.kt
@@ -1,26 +1,39 @@
+package com.r3.corda.lib.solana.bridging.token.test.driver
+
 import com.lmax.solana4j.api.PublicKey
+import com.lmax.solana4j.client.api.AccountInfo
+import com.lmax.solana4j.encoding.SolanaEncoding
 import com.lmax.solana4j.programs.AssociatedTokenProgram
 import com.r3.corda.lib.solana.bridging.token.flows.toPublicKey
+import com.r3.corda.lib.solana.bridging.token.flows.tokenProgramId
 import com.r3.corda.lib.solana.bridging.token.test.FlowTests.Companion.MSFT_TICKER
 import com.r3.corda.lib.solana.bridging.token.test.FlowTests.Companion.TOKEN_DECIMALS
 import com.r3.corda.lib.solana.bridging.token.test.SimpleDescriptor
 import com.r3.corda.lib.solana.bridging.token.test.TokenTypeDescriptor
 import com.r3.corda.lib.solana.bridging.token.testing.IssueSimpleTokenFlow
+import com.r3.corda.lib.solana.bridging.token.testing.QuerySimpleTokensFlow
 import com.r3.corda.lib.tokens.contracts.states.FungibleToken
 import com.r3.corda.lib.tokens.contracts.types.TokenType
-import com.r3.corda.lib.tokens.selection.tokenAmountWithIssuerCriteria
-import com.r3.corda.lib.tokens.workflows.utilities.rowsToAmount
-import com.r3.corda.lib.tokens.workflows.utilities.sumTokenCriteria
+import com.r3.corda.lib.tokens.contracts.utilities.sumTokenStatesOrThrow
+import com.r3.corda.lib.tokens.workflows.flows.rpc.MoveFungibleTokens
 import net.corda.core.contracts.Amount
+import net.corda.core.contracts.ContractState
+import net.corda.core.contracts.StateAndRef
 import net.corda.core.identity.CordaX500Name
+import net.corda.core.identity.Party
 import net.corda.core.messaging.startFlow
+import net.corda.core.messaging.vaultQueryBy
 import net.corda.core.node.NetworkParameters
 import net.corda.core.utilities.getOrThrow
+import net.corda.core.utilities.seconds
 import net.corda.solana.notary.common.Signer
+import net.corda.solana.notary.common.rpc.checkResponse
 import net.corda.solana.sdk.internal.Token2022
+import net.corda.testing.common.internal.eventually
 import net.corda.testing.core.ALICE_NAME
 import net.corda.testing.core.TestIdentity
 import net.corda.testing.driver.DriverParameters
+import net.corda.testing.driver.NodeHandle
 import net.corda.testing.driver.NodeParameters
 import net.corda.testing.driver.driver
 import net.corda.testing.node.NotarySpec
@@ -30,20 +43,34 @@ import net.corda.testing.solana.SolanaTestValidator
 import net.corda.testing.solana.randomKeypairFile
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNotNull
+import org.junit.jupiter.api.assertNull
 import org.junit.jupiter.api.io.TempDir
 import java.lang.IllegalStateException
+import java.math.BigDecimal
 import java.nio.file.Path
 import java.time.Duration
 import java.time.Instant
+import java.util.Base64
 import java.util.UUID
 
 class DriverTests {
+    companion object {
+        private val ISSUING_QUANTITY = BigDecimal("2000.000")
+        private val MOVE_QUANTITY = BigDecimal("10.250")
+    }
+
     private val msftDescriptor: TokenTypeDescriptor = SimpleDescriptor(MSFT_TICKER)
+    private lateinit var bridgeAuthorityParty: Party
+    private lateinit var bridgeAuthority: NodeHandle
     private lateinit var mintAuthoritySigner: Signer
     private lateinit var bridgeAuthoritySigner: Signer
     private lateinit var bridgeAuthorityWalletFile: Path
     private val aliceIdentity = TestIdentity(ALICE_NAME)
+    private lateinit var alice: NodeHandle
+    private lateinit var aliceParty: Party
     private val bridgeAuthorityIdentity = TestIdentity(CordaX500Name("Bridge Authority", "New York", "US"))
     private lateinit var aliceSigner: Signer
     private lateinit var aliceBridgeTokenAccount: PublicKey
@@ -138,6 +165,7 @@ class DriverTests {
                     NotarySpec(generalNotaryIdentity.name, validating = false),
                     NotarySpec(
                         solanaNotaryIdentity.name,
+                        false,
                         customConfig = mapOf<String, Any>(
                             "notary.validating" to false,
                             "notary.notaryLegalIdentity" to "$solanaNotaryName",
@@ -146,7 +174,7 @@ class DriverTests {
                             "notary.solana.custodiedKeysDir" to "$custodiedKeysDir",
                             "notary.solana.programWhitelist" to "[\"${Token2022.PROGRAM_ID}\"]",
                         ),
-                        validating = false,
+                        startInProcess = true,
                     ),
                 ),
                 networkParameters = NetworkParameters(
@@ -166,12 +194,13 @@ class DriverTests {
         ) {
             val user = User("user1", "test", permissions = setOf("ALL"))
 
-            val node = startNode(
+            alice = startNode(
                 providedName = aliceIdentity.name,
                 rpcUsers = listOf(user)
             ).getOrThrow()
+            aliceParty = alice.nodeInfo.legalIdentities.first()
 
-            val bridgeAuthorityNode = startNode(
+            bridgeAuthority = startNode(
                 NodeParameters(
                     providedName = bridgeAuthorityIdentity.name,
                     rpcUsers = listOf(user),
@@ -179,24 +208,131 @@ class DriverTests {
                     setOf(bridgingFlowsCordapp.withConfig(baConfig), bridgingContractsCordapp)
                 )
             ).getOrThrow()
+            bridgeAuthorityParty = bridgeAuthority.nodeInfo.legalIdentities.first()
 
-            val issuer = node.nodeInfo.legalIdentities.first()
+            val msftTokenType = alice.issue(msftDescriptor, ISSUING_QUANTITY, generalNotaryName)
 
-            val tokenType = TokenType("MSFT_TICKER", 3)
-            node.rpc
-                .startFlow(
-                    ::IssueSimpleTokenFlow,
-                    tokenType,
-                    100.toBigDecimal(),
-                    generalNotaryName,
-                ).returnValue
-                .getOrThrow()
+            assertNull(getAccountInfo(aliceBridgeTokenAccount), "Alice ATA should not be created yet")
 
-            val criteria = tokenAmountWithIssuerCriteria(tokenType, issuer).and(sumTokenCriteria())
+            val sig = alice.move(bridgeAuthorityParty, MOVE_QUANTITY, msftTokenType)
+            assertNotNull(sig)
 
-            val page = node.rpc.vaultQueryByCriteria(criteria, FungibleToken::class.java)
-            val balanceBefore: Amount<TokenType> = rowsToAmount(tokenType, page)
-            assertEquals(100000, balanceBefore.quantity)
+            val issuingBankParty = alice.nodeInfo.legalIdentities.first() // TODO
+            Thread.sleep(30.seconds.toMillis())
+            eventually(duration = 30.seconds) {
+                assertEquals(
+                    BigDecimal.ZERO,
+                    bridgeAuthority.myTokenBalance(issuingBankParty, msftTokenType),
+                    "Bridge Authority has no longer MSFT shares, they are under Locking Identity"
+                )
+            }
+            val msftFungibleToken = bridgeAuthority
+                .getAllFungibleTokens(issuingBankParty, msftTokenType)
+                .singleOrNull()
+            assertNotNull(msftFungibleToken, "There should be single MSFT fungible token in Bridge Authority vault")
+            assertTrue(
+                msftFungibleToken.holder !in setOf(aliceParty, bridgeAuthorityParty),
+                "Bridge Authority moved MSFT under Lock Identity (CI) " +
+                    "ownership as neither BA nor Alice holds the token",
+            ) // We don't know Confidential Identity upfront, so indirect check who doesn't have the token
+            assertEquals(
+                MOVE_QUANTITY,
+                msftFungibleToken.amount.toDecimal(),
+                "Lock Identity received expected number of MSFT shares",
+            )
+            val token: StateAndRef<FungibleToken>? = bridgeAuthority.queryStates<FungibleToken>().firstOrNull {
+                it.state.data.amount.token.tokenType == msftTokenType
+            }
+            assertNotNull(token)
+            val accountInfo = getAccountInfo(aliceBridgeTokenAccount)
+            assertAtaAccount(accountInfo, tokenMint, aliceSigner.account)
         }
+    }
+
+    fun NodeHandle.issue(
+        tokenDescriptor: TokenTypeDescriptor,
+        amount: BigDecimal,
+        notaryName: CordaX500Name,
+    ): TokenType {
+        val tokenType = TokenType(tokenDescriptor.ticker, tokenDescriptor.fractionDigits)
+        rpc
+            .startFlow(
+                ::IssueSimpleTokenFlow,
+                tokenType,
+                amount,
+                notaryName,
+            ).returnValue
+            .getOrThrow()
+        val myIdentity = nodeInfo.legalIdentities.first()
+        assertEquals(amount, myTokenBalance(myIdentity, tokenType))
+        return tokenType
+    }
+
+    private fun NodeHandle.getAllFungibleTokens(issuer: Party, stock: TokenType): List<FungibleToken> {
+        val fungibleTokenType1 = rpc
+            .startFlow(::QuerySimpleTokensFlow, issuer, stock)
+            .returnValue
+            .getOrThrow()
+            .firstOrNull()
+        val fungibleTokenType = fungibleTokenType1 ?: return emptyList()
+        return rpc
+            .vaultQueryBy<FungibleToken>()
+            .states
+            .map { it.state.data }
+            .filter {
+                it.amount.token == fungibleTokenType.state.data.issuedTokenType ||
+                    it.amount.token == fungibleTokenType.state.data.tokenType
+            }
+    }
+
+    fun NodeHandle.myTokenBalance(issuer: Party, tokenType: TokenType): BigDecimal {
+        val myIdentity = nodeInfo.legalIdentities.first()
+        val fungibleTokens = getAllFungibleTokens(issuer, tokenType).filter { it.holder == myIdentity }
+        return if (fungibleTokens.isEmpty()) {
+            BigDecimal.ZERO
+        } else {
+            fungibleTokens
+                .sumTokenStatesOrThrow()
+                .toDecimal()
+        }
+    }
+
+    private fun getAccountInfo(publicKey: PublicKey): AccountInfo? {
+        return testValidator
+            .client
+            .getAccountInfo(publicKey.base58(), testValidator.rpcParams)
+            .checkResponse("getAccountInfo")
+    }
+
+    private fun assertAtaAccount(
+        accountInfo: AccountInfo?,
+        expectedMintAccount: PublicKey,
+        expectedOwnerAccount: PublicKey,
+    ) {
+        assertNotNull(accountInfo, "Account not found")
+        assertEquals(accountInfo.owner, tokenProgramId.base58(), "ATA programId should match Token 2022")
+        val encodedInfo = accountInfo.data?.accountInfoEncoded
+        assertNotNull(encodedInfo, "Account data missing")
+        assertTrue(encodedInfo.size >= 2, "Missing encoded account data")
+        val binaryData = Base64.getDecoder().decode(encodedInfo.first())
+        val mintAccount = SolanaEncoding.account(binaryData.copyOfRange(0, 32))
+        assertEquals(mintAccount, expectedMintAccount, "ATA mint account mismatch")
+        val ownerAccount = SolanaEncoding.account(binaryData.copyOfRange(32, 64))
+        assertEquals(ownerAccount, expectedOwnerAccount, "ATA owner account mismatch")
+    }
+
+    private fun NodeHandle.move(toParty: Party, quantity: BigDecimal, tokenType: TokenType) =
+        rpc
+            .startFlow(
+                ::MoveFungibleTokens,
+                Amount.fromDecimal(quantity, tokenType),
+                toParty,
+            ).returnValue
+            .getOrThrow()
+
+    private inline fun <reified T : ContractState> NodeHandle.queryStates(): List<StateAndRef<T>> {
+        return rpc
+            .vaultQueryBy<T>()
+            .states
     }
 }
